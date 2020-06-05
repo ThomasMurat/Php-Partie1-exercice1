9dc773a040eacb770f1c45cde7c6417406306d2b
ThomasMurat <thomasmurat1992@gmail.com>
2020-06-05 11:11:27 +0200

    up exo1 partie1

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Php-Partie1-exercice1</title>
+    </head>
+    <body>
+        <?php 
+            $name = 'Thomas Murat';
+            echo "$name";
+        ?>
+    </body>
+</html>
